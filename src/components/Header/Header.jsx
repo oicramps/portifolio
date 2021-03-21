25dcf379e2257ebaeb5b9e7dc6b29869292815e6
Márcio Santos <marcio.tbms@gmail.com>
2021-03-21 20:49:37 -0300

    Updates the about me content

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,13 +12,12 @@ const Header = () => (
       <Title>Márcio Santos</Title>
       <SubTitle>Frontend Developer</SubTitle>
       <Description>
-        Since when I realized the impact that software has on people&apos;s lives and how much it is able to
-        significantly improve their lives, I started to give my best and at the same time love this career. I am a
-        frontend engineer who focuses on producing quality and well-tested code, mainly using Javascript and its variety
-        of libraries, where my repertoire is mainly composed of React, NodeJS, and Typescript. With these technologies,
-        I have been focusing on creating WEB applications with a consistent UI, easy to use and that give users
-        confidence. I am a person who plays with the team, with leadership and ownership instincts, but always in a good
-        mood and adaptable in relation to changes.
+        Since I realize the impact that software has on people&apos;s lives and how much it is able to significantly
+        improve their lives, I started to give my best and love this career at the same time. I am a frontend engineer
+        who focuses on producing quality and well-tested code, mainly using Javascript and its variety of libraries,
+        where my repertoire is mainly composed of React, NodeJS, and Typescript. With these technologies, I have been
+        focusing on creating reliable WEB applications with a consistent UI, and easy to use. I am a person who plays
+        with the team, with leadership and ownership instincts, but always in a good mood and resilient to changes.
       </Description>
     </DescriptionContainer>
   </Container>
