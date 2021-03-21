a1ca6ad131fbf1f8581b523d64889afaae427e75
Márcio Santos <marcio.pereira@mercadolivre.com>
2021-03-21 20:38:51 -0300

    Adds an about me content

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,9 +12,13 @@ const Header = () => (
       <Title>Márcio Santos</Title>
       <SubTitle>Frontend Developer</SubTitle>
       <Description>
-        Fusce tempor magna mi, non egestas velit ultricies nec. Aenean convallis, risus non condimentum gravida, odio
-        mauris ullamcorper felis, ut venenatis purus ex eu mi. Quisque imperdiet lacinia urna, a placerat sapien pretium
-        eu.
+        Since when I realized the impact that software has on people&apos;s lives and how much it is able to
+        significantly improve their lives, I started to give my best and at the same time love this career. I am a
+        frontend engineer who focuses on producing quality and well-tested code, mainly using Javascript and its variety
+        of libraries, where my repertoire is mainly composed of React, NodeJS, and Typescript. With these technologies,
+        I have been focusing on creating WEB applications with a consistent UI, easy to use and that give users
+        confidence. I am a person who plays with the team, with leadership and ownership instincts, but always in a good
+        mood and adaptable in relation to changes.
       </Description>
     </DescriptionContainer>
   </Container>
