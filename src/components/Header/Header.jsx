e0dc37a823a441daa26336ab0c5b8e52937dd3ba
Márcio Santos <marcio.pereira@mercadolivre.com>
2021-03-24 21:12:31 -0300

    Fixes typo on about me content

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,12 +12,12 @@ const Header = () => (
       <Title>Márcio Santos</Title>
       <SubTitle>Frontend Developer</SubTitle>
       <Description>
-        Since I realize the impact software has on people&apos;s lives and how much it can significantly improve it, I
-        started giving my best to this career with all my passion. I am a frontend engineer who focuses on producing
-        high-quality and well-tested code, mainly using Javascript and its variety of libraries, such as React, NodeJS,
-        and Typescript. Through these technologies, I have been focusing on creating reliable and easy-to-use WEB
-        applications with consistent UI. I am a team player always in a good mood and resilient to changes with
-        leadership and ownership instincts.
+        Since I realized the impact software has on people&apos;s lives and how much it can significantly improve it,
+        I&apos;ve started giving my best to this career with all my passion. I am a frontend engineer who focuses on
+        producing high-quality and well-tested code, mainly using Javascript and its variety of libraries, such as
+        React, NodeJS, and Typescript. Through these technologies, I have been focusing on creating reliable and
+        easy-to-use WEB applications with consistent UI. I am a team player always in a good mood and resilient to
+        changes with leadership and ownership instincts.
       </Description>
     </DescriptionContainer>
   </Container>
